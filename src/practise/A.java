5688c2bf05912abec98b6dd97fb78f0593464aae
Believer <er.nikhil1176@gmail.com>
2024-02-02 18:08:49 +0530

    java coding active

---
+++ src/practise/A.java
@@ -0,0 +1,16 @@
+package practise;
+
+public class A {
+	public static void m1() {
+		System.out.println("This is m1() method");	}
+	
+	public static void m2() {
+		System.out.println("this is m2() method");
+	}
+	
+	public static void main(String [] args) {
+		m1();
+		m2();
+	}
+
+}
